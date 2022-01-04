1bbadf6155a25a6c6b406e0a58140e3aa5de185c
K750i <munkin80@gmail.com>
2022-01-04 12:54:11 +0800

    Implement a different movement system

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,52 +1,43 @@
 extends KinematicBody2D
 
-export var ACCELERATION := 512
-export var MAX_SPEED := 64
 export var FRICTION := 0.25
-export var GRAVITY := 200
-export var JUMP_FORCE := 128
-export var MAX_SLOPE_ANGLE := 46
+export var GRAVITY := 800
+export var SPEED := Vector2(100, 200)
 
 var motion := Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
 	var input_vector := get_input_vector()
-	apply_horizontal_force(input_vector, delta)
-	apply_friction(input_vector)
-	jump_check()
-	apply_gravity(delta)
+	apply_horizontal_force(input_vector)
+	apply_gravity()
+	jump(input_vector)
 	move()
 
 
 func get_input_vector() -> Vector2:
-	var input_vector := Vector2.ZERO
-	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	return input_vector
+	return Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		-Input.get_action_strength("up") if Input.is_action_just_pressed("up") and is_on_floor() else 0.0
+	)
 
 
-func apply_horizontal_force(input_vector: Vector2, delta: float) -> void:
+func apply_horizontal_force(input_vector: Vector2) -> void:
 	if input_vector.x != 0:
-		motion.x += input_vector.x * ACCELERATION * delta
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		motion.x = input_vector.x * SPEED.x
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)		
 
 
-func apply_friction(input_vector: Vector2) -> void:
-	if input_vector.x == 0:
-		motion.x = lerp(motion.x, 0, FRICTION)
+func apply_gravity() -> void:
+	motion.y += GRAVITY * get_physics_process_delta_time()
 
 
-func apply_gravity(delta: float) -> void:
-	motion.y += GRAVITY * delta
-	motion.y = min(motion.y, JUMP_FORCE)
-
-
-func jump_check() -> void:
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		motion.y = -JUMP_FORCE
-	if Input.is_action_just_released("up") and motion.y < 0:
-		motion.y = 0
-
+func jump(input_vector: Vector2) -> void:
+	if input_vector.y < 0.0:
+		motion.y = input_vector.y * SPEED.y
+	if Input.is_action_just_released("up") and motion.y < 0.0:
+		motion.y = 0.0
 
 func move() -> void:
 	motion = move_and_slide(motion, Vector2.UP)
